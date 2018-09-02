d2b95817090aa2e0b020fd187bd10b4378032923
Rohan Paleja <rohanpaleja123@gmail.com>
2018-09-02 10:51:01 -0400

    first commit

---
+++ Lab_1/ball_tracking.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import cv2
+import numpy as np 
+
+cap =cv2.VideoCapture(0)
+
+while (True):
+    #Capture frame by frame
+    ret, frame = cap.read(0)
+
+    #Our operations on the frame come here
+    # gray =cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
